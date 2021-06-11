daeca96eb8adf7ca73e2c68adbb2d5e7cccf0e19
Tu nombre <julicmrgo@gmail.com>
2021-06-11 15:57:33 -0300

    Ejercicio 2 RTOS

--- EJ2_RTOS/src/main.c
+++ EJ2_RTOS/src/main.c
@@ -43,12 +43,13 @@ static void vTarea1(void *pvParameters){
      vTaskDelay(500 / portTICK_RATE_MS);
      state = Board_GPIO_GetStatus(BOARD_GPIO_2);
      printf("Estado de la entrada GPIO_2: %d\r\n", state);
-     if state{
+     if ( state = 1 ){
        xTaskCreate(vTarea2, (const char *)"Tarea2", TAM_PILA, (void*)pcTextoTarea2, tskIDLE_PRIORITY+2, NULL );
+      }
      }
-     }
-   }
 }
+
+if
 
 static void vTarea2( void *pvParameters){
     Board_LED_Toggle(LED_1);  //Led amarillo
