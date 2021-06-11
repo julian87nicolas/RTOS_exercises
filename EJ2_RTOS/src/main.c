7065e841a11a3eea35c0620a23d43039b2a47020
Tu nombre <julicmrgo@gmail.com>
2021-06-11 19:01:42 -0300

    Creo que ya anda, la puta madre

--- EJ2_RTOS/src/main.c
+++ EJ2_RTOS/src/main.c
@@ -37,22 +37,23 @@ const char *pcTextoTarea2 = "Tarea2 is running\r\n";
 
 /*==================[internal functions definition]==========================*/
 
-static void vTarea2( void *pvParameters){
-    Board_LED_Toggle(LED_1);
-    printf("Estado de LED amarillo: %d\r\n", gpioRead(LED_1));
-    vTaskDelay(100/portTICK_RATE_MS);
-    for(;;){
-
+static void vTarea2( void *pvParameters){ //La Tarea2 se deeclara antes para que no haya error en la compilacion.
+  for(;;){
+      Board_LED_Toggle(LED_1);              //Cambia el valor de LED_1
+      printf("Estado de LED amarillo: %d\r\n", gpioRead(LED_1));  //Lo muestra en puerto serie
+      vTaskDelay(100/portTICK_RATE_MS);   //Delay para permitir activar o descativar individualmente el led
     }
 }
 
 static void vTarea1(void *pvParameters){
-   for ( ;; ){
-     vTaskDelay(500 / portTICK_RATE_MS);
-     printf("Estado de la entrada GPIO_2: %d\r\n", Board_GPIO_GetStatus(BOARD_GPIO_2));
+  bool status;
+  for ( ;; ){
+     vTaskDelay(500 / portTICK_RATE_MS);                    //Delay de 500ms
+     status = Board_GPIO_GetStatus(BOARD_GPIO_2);           //Lee la entrada GPIO_2
+     printf("Estado de la entrada GPIO_2: %d\r\n", status); //La imprime en puerto serie
 
-      if (Board_GPIO_GetStatus(BOARD_GPIO_2)){
-        xTaskCreate(vTarea2, (const char *)"Tarea2", TAM_PILA, (void*)pcTextoTarea2, tskIDLE_PRIORITY+1, NULL );
+      if (status)){              //Si está en alto, comienza la Tarea2.
+        xTaskCreate(vTarea2, (const char *)"Tarea2", TAM_PILA, (void*)pcTextoTarea2, tskIDLE_PRIORITY+1, NULL );  //Arranca la tarea con prioridad 1 + la minima, osea menor a la de Tarea1
       }
      }
 }
@@ -64,11 +65,12 @@ static void vTarea1(void *pvParameters){
 int main(void)
 {
   Board_Init();
-	xTaskCreate(vTarea1, (const char *)"Tarea1", TAM_PILA, (void*)pcTextoTarea1, tskIDLE_PRIORITY+2, NULL );
 
-	vTaskStartScheduler(); /* y por último se arranca el planificador . */
-    //Nunca llegara a ese lazo  .... espero
-     for( ;; );
+  xTaskCreate(vTarea1, (const char *)"Tarea1", TAM_PILA, (void*)pcTextoTarea1, tskIDLE_PRIORITY+2, NULL ); //Arranca la tarea dos con prioridad 2 + la mínima
+
+	vTaskStartScheduler(); //Arranca el planificador
+
+  for( ;; );
      return 0;
 }
 
