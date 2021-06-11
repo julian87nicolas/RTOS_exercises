d740b881ab218e70b649ef6a248facd02ca2e636
Tu nombre <julicmrgo@gmail.com>
2021-06-11 15:17:24 -0300

    Ejercicio 3 RTOS

---
+++ EJ3_RTOS/src/main.c
@@ -0,0 +1,78 @@
+/* 3 - Cree un programa usando FreeRtos que cumpla con las siguientes consignas:
+
+Ejecutar dos tareas, Tarea1 y Tarea2.
+*La Tarea1, de mayor prioridad, debe ejecutar una espera activa (busy-waiting)
+indicando a través del puerto serie el valor de las entradas GPIO_0 a GPIO_3
+en formato binario (nibble).
+*Periódicamente debe aumentar la prioridad de la Tarea2, para que esta pueda ejecutarse.
+*La Tarea2 debe cambiar el estado del led verde, indicarlo por puerto serie y
+disminuir su propia prioridad para detener su ejecución.
+  */
+
+  /*
+
+  LED_1 = amarillo
+  LED_2 = rojo
+  LED_3 = verde
+
+  */
+ /*==================[inclusions]=============================================*/
+
+#include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*==================[macros and definitions]=================================*/
+
+#define TAM_PILA 150
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+const char *pcTextoTarea1 = "Tarea1 is running\r\n";
+const char *pcTextoTarea2 = "Tarea2 is running\r\n";
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+static void vTarea1(void *pvParameters){
+  int state0, state1, state2, state3;
+  //uint8 state0, state1, state2, state3;
+  //nibble state0, state1, state2, state3;
+   for ( ;; ){
+     vTaskDelay(500 / portTICK_RATE_MS);
+     state0 = Board_GPIO_GetStatus(BOARD_GPIO_0);
+     state1 = Board_GPIO_GetStatus(BOARD_GPIO_1);
+     state2 = Board_GPIO_GetStatus(BOARD_GPIO_2);
+     state3 = Board_GPIO_GetStatus(BOARD_GPIO_3);
+     print("\r\nEstado de la entrada GPIO_0 GPIO_1 GPIO_2 GPIO_3:\r\n");
+     print("\r\n%d, %d, %d, %d", state0, state1, state2, state3);
+     uxTaskPriority(128);
+     }
+   }
+}
+
+static void vTarea2( void *pvParameters){
+    Board_LED_Toggle(LED_3);  //Verde
+    printf("Estado de LED amarillo cambiado a: %d\r\n", gpioRead(LED_3) );
+    vTaskPrioritySet(128, tskIDLE_PRIORITY+1);
+
+}
+/*==================[external functions definition]==========================*/
+
+int main(void)
+{
+  Board_Init();
+	xTaskCreate(vTarea1, (const char *)"Tarea1", TAM_PILA, (void*)pcTextoTarea1, tskIDLE_PRIORITY+2, NULL );
+  xTaskCreate(vTarea2, (const char *)"Tarea1", TAM_PILA, (void*)pcTextoTarea1, tskIDLE_PRIORITY+1, 128 );
+
+	vTaskStartScheduler(); /* y por último se arranca el planificador . */
+    //Nunca llegara a ese lazo  .... espero
+     for( ;; );
+     return 0;
+}
+
+/*==================[end of file]============================================*/
