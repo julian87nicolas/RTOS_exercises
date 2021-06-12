9680b41fbb6eecfcb650183ab41efaec0aeae516
Tu nombre <julicmrgo@gmail.com>
2021-06-12 19:46:03 -0300

    Commit innumerable

--- EJ2_RTOS/src/main.c
+++ EJ2_RTOS/src/main.c
@@ -48,7 +48,7 @@ static void vTarea2( void *pvParameters){ //La Tarea2 se declara antes para que 
     Board_LED_Toggle(LED_1);
     printf("Estado de LED amarillo: %d\r\n", gpioRead(LED_1));  //Lo muestra en puerto serie
     vTaskDelay(500/portTICK_RATE_MS);
-    vTaskPrioritySet( &idTarea2, tskIDLE_PRIORITY+1 );     //Disminuye la prioridad de la tarea actual para que en el proximo tick se ejecute la tarea1
+    vTaskPrioritySet( NULL, tskIDLE_PRIORITY+1 );     //Disminuye la prioridad de la tarea actual para que en el proximo tick se ejecute la tarea1
     //TaskEndTrace();
     //vTaskDelete(vTarea2); //Delay para permitir activar o descativar individualmente el led
   }
@@ -64,12 +64,12 @@ static void vTarea1(void *pvParameters){
      status = Board_GPIO_GetStatus(BOARD_GPIO_2);           //Lee la entrada GPIO_2
      printf("Estado de la entrada GPIO_2: %d\r\n", status); //La imprime en puerto serie
      if (status){
-       if(t==0){
-         t=1;
+       if(!t){
+         t = true;
          xTaskCreate(vTarea2, (const char *)"Tarea2", TAM_PILA, (void*)pcTextoTarea2, uxPriority1 + 1, &idTarea2 );  //Arranca la tarea con prioridad mayor a tarea1 asi aranca de inmediato
         }
         else{
-          vTaskPrioritySet( &idTarea2 ,  uxPriority1+1 );
+          //vTaskPrioritySet( &idTarea2 ,  uxPriority1+1 );
         }
       }
     }
