5c22973acc117e07f824df7c157e1e99406329dd
Tu nombre <julicmrgo@gmail.com>
2021-06-15 00:58:53 -0300

    AlFIN

--- EJ4_RTOS/src/main.c
+++ EJ4_RTOS/src/main.c
@@ -55,6 +55,7 @@ void vApplicationIdleHook( void ){
       xLastWakeTime = xTaskGetTickCount();
       printf("\r\nIdle Hook en funcionamiento");
       Board_LED_Toggle(LED_BLUE);
+      printf("\r\nEstado de LED cambiado a: %b", Board_LED_Test(LED_BLUE));
     }
 
 }
