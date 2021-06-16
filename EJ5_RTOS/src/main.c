f0d9010e3a71aa0102edcb78b2fe130f3bbc322e
Tu nombre <julicmrgo@gmail.com>
2021-06-16 15:49:41 -0300

    masda

--- EJ5_RTOS/src/main.c
+++ EJ5_RTOS/src/main.c
@@ -26,7 +26,7 @@ QueueHandle_t cola_msj;
 static void vTareaEnvia(void *pvParameters){
   int TiempoON, *LedON;
   BaseType_t Escr;
-  LedON = (void*)pvParameters;   //Aparece un warning acá. Cuidado
+  *LedON = (void*)pvParameters;   //Aparece un warning acá. Cuidado
 
   TiempoON = LedON / portTICK_RATE_MS;
 
